1f3df03634c68029697f6e09d665d632fac2d46b
Tonmoy-Roy <emonismail69@gmail.com>
2022-11-25 15:57:10 +0600

    product setup done

--- src/layout/Dashboardlayout/MyProduct.js
+++ src/layout/Dashboardlayout/MyProduct.js
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react';
-import {AuthContext} from '../../contexts/UserContext'
+import { AuthContext } from '../../contexts/UserContext'
 import { useQuery } from '@tanstack/react-query';
 
 const MyProduct = () => {
-    const {user}=useContext(AuthContext);
-    const url=`htpp...`
+    const { user } = useContext(AuthContext);
+    const url = `http://localhost:5000/bookings`
     const { data: bookings = [] } = useQuery({
         queryKey: ['bookings', user?.email],
         queryFn: async () => {
             const res = await fetch(url, {
                 headers: {
-                   authorization: `bearer ${localStorage.getItem('accessToken')}` 
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
                 }
             });
             const data = await res.json();
@@ -20,40 +20,80 @@ const MyProduct = () => {
 
     return (
         <div>
-        <h3 className="text-3xl mb-5">My Products</h3>
-        <div className="overflow-x-auto">
-            <table className="table w-full">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Location</th>
-                        <th>Resale Price</th>
-                        <th>Years of use</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        bookings.map((booking, i) => <tr key={booking._id}>
-                            <th>{i+1}</th>
-                            <td>{booking.
-title}</td>
-                            <td>{booking.
+            <h3 className="text-3xl mb-5">My Products</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th>Resale Price</th>
+                            <th>Mobile Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bookings.map((booking, i) => <tr key={booking._id}>
+                                <th>{i + 1}</th>
+                                <td>{booking.
+                               mobileName}</td>
 
-location
-}</td>
-                            <td>{booking.resalePrice
+                                <td>{booking.
 
-}</td>
-                            {/* <td>{booking.
+                                    meetLocation
+                                }
+                                </td>
+                                <td>{booking.
 
-years of use}</td> */}
-                        </tr>)
-                    }
-                </tbody>
-            </table>
+price
+                                }
+                                </td>
+
+                                <td>{booking.phone
+
+                                }</td>
+                                
+                                <td><div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src={booking.img} />
+  </div>
+</div></td>
+
+
+
+{/* 
+<div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://placeimg.com/192/192/people" />
+  </div>
+</div>
+
+{booking.
+
+phone} */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     );
 };
 
